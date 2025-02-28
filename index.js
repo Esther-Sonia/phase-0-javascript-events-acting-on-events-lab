f0fa7e40b82739a0f29f8d72dd3e4642bc83541f
Esther-Sonia <esthersonia21@gmail.com>
2025-02-28 10:27:02 +0300

    implements move functions

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+const dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  let left = parseInt(dodger.style.left.replace('px', '')) || 0;
+
+  if (left > 0) {
+    dodger.style.left = `${left - 10}px`;
+  }
+}
+
+function moveDodgerRight() {
+  let left = parseInt(dodger.style.left.replace('px', '')) || 0;
+
+  if (left < 360) {
+    dodger.style.left = `${left + 10}px`;
+  }
+}
+
+
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'ArrowLeft') {
+    moveDodgerLeft();
+  } else if (event.key === 'ArrowRight') {
+    moveDodgerRight();
+  }
+});
